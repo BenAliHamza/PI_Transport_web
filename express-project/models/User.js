7b395ffe1bb53247b5f26ec5683609a7ad78e439
oussamadridii <dridi.oussama@esprit.tn>
2024-05-17 20:16:57 +0100

    adjustements to user

--- express-project/models/User.js
+++ express-project/models/User.js
@@ -1,11 +1,32 @@
-const mongoose = require('mongoose')
-const user_schema = new mongoose.Schema({
+// needed packages Import
+const mongoose = require("mongoose");
+const {EMAIL_REGEX, PASSWORD_REGEX} = require("../shared/commonService");
+const { Schema } = mongoose;
 
-    username : {required : true  , type : String , default : "default username"}
-    , 
-    password : {
-        required : true  , type : String 
-    }
-})
+// user schema
 
-module.exports = mongoose.model("User", user_schema)
+const userSchema = new Schema(
+  {
+    pseudo: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: EMAIL_REGEX,
+    },
+    password: {
+      type: String,
+      required: true ,
+      match: PASSWORD_REGEX,
+    },
+    phone: {type : Number , required :false },
+    image: {type : String , required : false },
+  },
+  { versionKey: false, timestamps: true }
+);
+// create the user model
+const User = mongoose.model("Users", userSchema);
+
+module.exports = User;
