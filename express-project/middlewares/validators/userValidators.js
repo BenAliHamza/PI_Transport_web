6bb79dc848bc8af8914512ddef84f5506faa0572
Ben Ali Hamza <132414715+BenAliHamza@users.noreply.github.com>
2024-05-23 18:52:13 +0100

    User validators (#4)
    user model and validators

---
+++ express-project/middlewares/validators/userValidators.js
@@ -0,0 +1,36 @@
+// userValidation.js
+const { body, validationResult } = require('express-validator');
+const {EMAIL_REGEX, PASSWORD_REGEX} = require("../../shared/services/commonService");
+
+const validateUser = [
+  body('pseudo')
+    .notEmpty().withMessage('Pseudo is required.')
+    .isString().withMessage('Pseudo must be a string.'),
+  body('firstname')
+    .notEmpty().withMessage('First name is required.')
+    .isString().withMessage('First name must be a string.'),
+  body('lastname')
+    .notEmpty().withMessage('Last name is required.')
+    .isString().withMessage('Last name must be a string.'),
+  body('email')
+    .notEmpty().withMessage('Email is required.')
+    .matches(EMAIL_REGEX).withMessage('Email is not valid.'),
+  body('password')
+    .notEmpty().withMessage('Password is required.')
+    .matches(PASSWORD_REGEX).withMessage('Password is not valid.'),
+  body('phone')
+    .optional()
+    .isNumeric().withMessage('Phone must be a number.'),
+  body('image')
+    .optional()
+    .isString().withMessage('Image must be a string.'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(403).json (errors);
+    }
+    next();
+  }
+];
+module.exports = { validateUser };
--- express-project/routes/users.js
+++ express-project/routes/users.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const {validateUser} = require("../middlewares/validators/userValidators");
+const User = require("../models/userModel");
 var router = express.Router();
 
 /* GET users listing. */
@@ -6,4 +8,19 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.post('/'  ,validateUser , async (req, res, next)=>{
+    const body = req.body
+    try {
+    await  User.create(req.body)
+    }catch (e){
+      res.status(402).json({error: e});
+    }
+})
+
+router.get('/' , validateUser  , async (req, res, next)=>{
+return {
+  message : 'respond with a resource'
+}
+})
 module.exports = router;
