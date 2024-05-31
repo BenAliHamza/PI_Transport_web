a591ab8eae10ecf08d0c99083a0e2bd23962a378
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2024-05-31 23:46:58 +0200

    ajout module reclamation reponse (#11)
    * Modification1
    
    * Modification2
    
    Model Reclamation
    Model Reponse
    
    * Modification3
    
    Mise à jour code
    
    * Ajout 30/05/24
    
    * version 2

--- express-project/app.js
+++ express-project/app.js
@@ -7,6 +7,10 @@ require('dotenv').config()
 var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var reclamationRouter = require('./routes/Reclamation');
+var reponseRouter = require('./routes/Reponse');
+const Reponse = require('./models/Reponse');
+
 var accessoireRoutes = require('./routes/accessoireRoutes');
 const categorieFavorieRoutes = require('./routes/categorieFavorieRoutes');
 var categorieAccessoireRoutes = require('./routes/categorieAccessoireRoutes');
@@ -34,6 +38,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/accessoires', accessoireRoutes);
 app.use('/categories', categorieAccessoireRoutes);
 app.use('/', indexRouter);
+app.use('/reclamations', reclamationRouter);
+app.use('/reponses', reponseRouter);
 app.use('/users', usersRouter);
 app.use('/categorie-favories', categorieFavorieRoutes);
 app.use('/annonce', annonceRouter);
@@ -41,9 +47,27 @@ app.use('/reservation', reservationRouter);
 
 
 // catch 404 and forward to error handler
+<<<<<<< HEAD
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(5000);
+=======
 app.use(notFoundError);
 app.use(errorHandler);
 
+>>>>>>> main
 
 app.listen(5000);
 module.exports = app;
