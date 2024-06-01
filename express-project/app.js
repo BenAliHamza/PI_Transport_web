6666a12e49171634a4f2480f82ef0ba0025abc7b
Jridi Seif eddine <133594071+SeifEddineJridi@users.noreply.github.com>
2024-06-01 09:58:22 +0100

    Update app.js

--- express-project/app.js
+++ express-project/app.js
@@ -13,6 +13,8 @@ var connectionString = process.env.CONNECTION_STRING ?? "mongodb://localhost:270
 var reclamationRouter = require('./routes/Reclamation');
 var reponseRouter = require('./routes/Reponse');
 const Reponse = require('./models/Reponse');
+const CategorieAccessoire = require('./models/categorieAccessoire');
+const CategorieFavorie = require('./models/CategorieFavorie');
 
 var accessoireRoutes = require('./routes/accessoireRoutes');
 const categorieFavorieRoutes = require('./routes/categorieFavorieRoutes');
